3b07d6a685a62ea1784e0b00a4d8e1a7074fea35
Stephan <hi@polarizedions.net>
2024-08-17 12:01:22 +0200

    update latest version

--- version.nix
+++ version.nix
@@ -1,4 +1,4 @@
 {
-  version = "3.13.0";
-  sha256 = "e89f3bb7fc24c148e3044b807774fcfe05e7eae9e551863a38a2dcd7e0ac05f1";
+  version = "3.15.0";
+  sha256 = "5cf3eb08de96821b0ad3d0c93174b2e308041d5490a3edb772dfd89a6d89d012";
 }
